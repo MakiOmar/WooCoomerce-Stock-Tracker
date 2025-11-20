36954b76e3fa7b532026a7c0b854fd931dfdfd49
TaskFlow Developer <developer@taskflow.app>
2025-11-20 08:30:53 +0200

    Remove non-standard header field

--- anony-stock-log.php
+++ anony-stock-log.php
@@ -14,8 +14,6 @@
  * WC tested up to: 8.0
  * License: GPL v2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
- * Network: false
- * GitHub Plugin URI: MakiOmar/WooCoomerce-Stock-Tracker
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
